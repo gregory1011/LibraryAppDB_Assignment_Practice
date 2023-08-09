e83a068d0583ed098f2fd4492517853f7f42adc9
grigorerosca <gregory081093@gmail.com>
2023-08-09 14:03:55 -0400

    hooks - updated

--- src/test/java/com/library/steps/Hooks.java
+++ src/test/java/com/library/steps/Hooks.java
@@ -1,6 +1,7 @@
 package com.library.steps;
 
 import com.library.utility.ConfigurationReader;
+import com.library.utility.DB_Util;
 import com.library.utility.Driver;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
@@ -12,9 +13,10 @@ import java.util.concurrent.TimeUnit;
 
 public class Hooks {
 
-    @Before
+
+    @Before ("@ui")
     public void setUp(){
-        System.out.println("this is coming from BEFORE");
+        System.out.println("this is coming from BEFORE - UI start connection");
         Driver.getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         Driver.getDriver().manage().window().maximize();
         Driver.getDriver().get(ConfigurationReader.getProperty("library_url"));
@@ -22,9 +24,9 @@ public class Hooks {
 
     }
 
-    @After
+    @After ("@ui")
     public void tearDown(Scenario scenario){
-        System.out.println("this is coming from AFTER");
+        System.out.println("this is coming from AFTER - UI close connection");
 
         if(scenario.isFailed()){
             final byte[] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
@@ -33,6 +35,18 @@ public class Hooks {
 
         Driver.closeDriver();
 
+    }
+
+
+    @Before ("@db")
+    public void setupDB(){
+        System.out.println("Connecting to database...");
+        DB_Util.createConnection();
+    }
+
+    @After ("@db")
+    public void closeDB(){
+        System.out.println("Closing DB connection");
     }
 
 
